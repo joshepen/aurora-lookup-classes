506e1b9cb8891538e99b43f2c49c7cead4cba412
joshepen <pennerthing2@gmail.com>
2024-07-07 21:36:19 -0500

    Finish code with bug
    Lots was done since last commit, the whole process now works, except the course catalog descriptions are sometimes wrong because some elements aren't being clicked so I'm going to fix that still.

--- AuroraLookupClasses.py
+++ AuroraLookupClasses.py
@@ -1,28 +1,51 @@
 from bs4 import BeautifulSoup
 from auroranav.AuroraNav import AuroraNav
 import os
+import openpyxl as xl
 import threading
 
-
-class CourseInfo:
-    def __init__(self):
-        pass
+INPUT_DIR = "input/"
+OUTPUT_DIR = "output/"
 
 
 def main():
-    semester = GetSemester("courses.txt")
+    i = 1
+    files = os.listdir(INPUT_DIR)
+    filename = "courses%d.txt" % i
+    while filename in files:
+        LookupClasses(INPUT_DIR + filename)
+        i += 1
+        filename = "courses%d.txt" % i
 
-    names = GetClassNames("courses.txt")
 
-    nav = AuroraNav()
+def LookupClasses(filename):
+    semester = GetSemester(filename)
+    names = GetClassNames(filename)
+    userID = GetUserID(filename)
+    password = GetPassword(filename)
+
+    nav = AuroraNav(headless=False)
     nav.OpenAurora()
-    nav.Login(os.getenv("AURORA_USER"), os.getenv("AURORA_PASS"))
+    nav.Login(userID, password)
+    data = []
     for name in names:
+        data.append(GetClassInfo(nav, semester, name))
 
-        # x = threading.Thread(target=GetClassInfo, args=(semester, name))
-        # x.start()
-        GetClassInfo(nav, semester, name)
+    CourseInfoToSpreadsheet(OUTPUT_DIR + semester + ".xlsx", data)
+
     nav.CloseWindow()
+
+
+def GetUserID(filepath):
+    userIDIndex = 0
+    with open(filepath, "r") as file:
+        return file.readlines()[userIDIndex].strip()
+
+
+def GetPassword(filepath):
+    passwordIndex = 1
+    with open(filepath, "r") as file:
+        return file.readlines()[passwordIndex].strip()
 
 
 def GetCatalogInfo(nav: AuroraNav, semester, courseName):
@@ -33,11 +56,14 @@ def GetCatalogInfo(nav: AuroraNav, semester, courseName):
 
     nav.SelectTerm(semester)
     nav.SelectDepartment(courseName.split(" ")[0])
-    nav.GoToPage(courseName)
-    return nav.GetPageContents()
+    if nav.GoToPage(courseName):
+        return ProcessCatalogPage(
+            BeautifulSoup(nav.GetPageContents(), features="html.parser")
+        )
+    return None
 
 
-def GetLUInfo(nav: AuroraNav, semester, name):
+def GetLUCInfo(nav: AuroraNav, semester, name):
     nav.HomePage()
     nav.GoToPage("Enrolment & Academic Records")
     nav.GoToPage("Registration and Exams")
@@ -45,35 +71,60 @@ def GetLUInfo(nav: AuroraNav, semester, name):
 
     nav.SelectTerm(semester)
     nav.SelectDepartment(name.split(" ")[0])
-    nav.GoToLookupClass(name.split(" ")[1])
-
-    content = BeautifulSoup(nav.GetPageContents(), features="html.parser")
-    content = ProcessCourseInfoPage(content)
-    return content
+    if nav.GoToLookupClass(name.split(" ")[1]):
+        content = BeautifulSoup(nav.GetPageContents(), features="html.parser")
+        content = ProcessLUCPage(content)
+        return content
+    return []
 
 
 def GetSemester(filename):
+    semesterIndex = 2
     with open(filename, "r") as file:
-        return file.readline().strip()
+        return file.readlines()[semesterIndex].strip()
 
 
 def GetClassNames(filename):
+    classesIndex = 3
     names = []
     with open(filename, "r") as file:
-        for line in file.readlines()[1:]:
+        for line in file.readlines()[classesIndex:]:
             names.append(line.strip())
     return names
 
 
-def ProcessCourseInfoPage(pageContents: BeautifulSoup):
-    table = pageContents.find("table", class_="datadisplaytable")
-    GetLUTableData(table)
+def ProcessLUCPage(pageContents: BeautifulSoup):
+    table = pageContents.find("table", class_="datadisplaytable").tbody
 
-
-def GetLUTableData(table: BeautifulSoup):
-    tbl = table.tbody
     data = []
-    wantedColumns = GetWantedColumnIndices(tbl.contents[2])
+    wantedColumns = GetWantedColumnIndices(table.contents[2])
+
+    currItem = {}
+    for name in wantedColumns:
+        currItem[name] = []
+
+    for trIndex in range(3, len(table.contents)):
+        if (
+            table.contents[trIndex] != "\n"
+            and table.contents[trIndex].find("b") == None
+            and table.contents[trIndex].find("hr") == None
+        ):
+            for columnName in wantedColumns:
+                columnIndex = wantedColumns[columnName]
+                value = table.contents[trIndex].contents[columnIndex].string
+                currItem[columnName].append(value)
+
+        elif table.contents[trIndex].find("hr") not in [None, -1]:
+            data.append(currItem)
+            currItem = {}
+            for name in wantedColumns:
+                currItem[name] = []
+
+    return data
+
+
+def ProcessCatalogPage(page: BeautifulSoup):
+    return page.find("td", class_="ntdefault").get_text()
 
 
 def GetWantedColumnIndices(headerRow: BeautifulSoup):
@@ -85,21 +136,73 @@ def GetWantedColumnIndices(headerRow: BeautifulSoup):
         "Days",
         "Time",
         "Instructor",
-        "Date (MM/DD)",
+        "Date",
+        "Rem",
     ]
     wantedIndices = {}
     for i in range(len(headerRow.contents)):
-        print(headerRow.contents[i].string)
-        if headerRow.contents[i].string in wantedColumns:
-            wantedIndices[headerRow.contents[i].string] = i
+        for name in wantedColumns:
+            if name in headerRow.contents[i].get_text():
+                wantedIndices[headerRow.contents[i].get_text()] = i
 
     return wantedIndices
 
 
 def GetClassInfo(nav: AuroraNav, semester, name):
+    data = {}
+    data["table"] = GetLUCInfo(nav, semester, name)
+    data["description"] = GetCatalogInfo(nav, semester, name)
+    data["name"] = name
+    return data
 
-    description = GetCatalogInfo(nav, semester, name)
-    table = GetLUInfo(nav, semester, name)
+
+def CourseInfoToSpreadsheet(filepath, data: list[dict]):
+    wb = xl.Workbook()
+    sortedData = sorted(data, key=CourseDataSortFn)
+    for course in sortedData:
+        ws = wb.create_sheet(course["name"])
+        ws["A1"].value = course["description"]
+        if len(course["table"]) > 0:
+            rowIndex = 2
+            colIndex = 1
+            # Create header row
+            for header in course["table"][0]:
+                ws.cell(row=rowIndex, column=colIndex).value = header
+                colIndex += 1
+            rowIndex += 1
+            # Add data per section (time)
+            for section in course["table"]:
+                colIndex = 1
+                for header in section:
+                    for i in range(len(section[header])):
+                        ws.cell(row=rowIndex + i, column=colIndex).value = section[
+                            header
+                        ][i]
+                    colIndex += 1
+                rowIndex += 1
+    wb.remove(wb["Sheet"])
+    wb.save(filepath)
 
 
-main()
+def CourseDataSortFn(course: dict):
+    if len(course["table"]) == 0:
+        return str(
+            [
+                0xFF,
+                0xFF,
+                0xFF,
+                0xFF,
+                0xFF,
+                0xFF,
+                0xFF,
+                0xFF,
+                0xFF,
+                0xFF,
+            ]
+        )
+    else:
+        return course["name"]
+
+
+if __name__ == "__main__":
+    main()
