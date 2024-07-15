73db5791fa4bae1b7bc63c31b56966d828dec33f
joshepen <pennerthing2@gmail.com>
2024-07-14 19:54:27 -0500

    Add multiple output options
    Right now it just defaults to a string (and outputs to file) but when I make a GUI, I'll add the option to choose output. I also extracted the course data to a class to allow easier output.

---
+++ out/OutputString.py
@@ -0,0 +1,28 @@
+from out.IOutput import IOutput
+from objects.Course import Course
+import os
+from tabulate import tabulate
+import itertools
+
+
+class OutputString(IOutput):
+
+    @staticmethod
+    def output(data: list[Course], filepath: str = None) -> str:
+        string = ""
+        for course in data:
+            string += f"{course.GetName()}\n{course.GetDescription()}\n"
+            if len(course) > 0:
+                string += tabulate(
+                    itertools.chain.from_iterable(course.GetSections()),
+                    headers=course.GetHeaders(),
+                )
+            string += "\n\n"
+
+        if filepath != None:
+            if not os.path.exists(filepath[: filepath.rfind("/")]):
+                os.mkdir(filepath[: filepath.rfind("/")])
+            with open(filepath, "w") as file:
+                file.write(string)
+
+        return string
